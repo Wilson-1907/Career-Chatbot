f30b7511897ba14bee38b71258b28cad9700e82f
Techflare <wkabucho4@gmail.com>
2025-08-27 15:38:05 +0300

    chat.py

--- CBE chatbot backend/chat.py
+++ CBE chatbot backend/chat.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from models.chat_models import ChatMessage, ChatResponse
-from services.chat_service import handle_chat
+from chat_models import ChatMessage, ChatResponse
+from chat_service import handle_chat
 
 router = APIRouter(prefix="/chat", tags=["Chat"])
+
 
 @router.post("", response_model=ChatResponse)
 def chat(payload: ChatMessage):
