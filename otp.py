0ad8197f0af6bc3c7c0c9c2a0530dc932c659aec
Techflare <wkabucho4@gmail.com>
2025-09-02 06:48:08 +0300

    Delete otp.py

--- otp.py
+++
@@ -1,8 +0,0 @@
-import random
-
-
-def generate_otp(length: int = 4) -> str:
-    """Return a numeric OTP as a string, e.g., '4831'."""
-    start = 10 ** (length - 1)
-    end = (10 ** length) - 1
-    return str(random.randint(start, end))
